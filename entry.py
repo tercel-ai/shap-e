fdcb9f8ea7e36f1cc7abd915a9ee8ef1c5393636
tercel <tercel.yi@gmail.com>
2023-05-30 14:14:07 +0800

    fixed upload_file, when file.read to end then save 0 size

--- entry.py
+++ entry.py
@@ -191,14 +191,15 @@ def upload_file(file):
     if ext.lower() not in ['.bmp','.png','.jpg','.jpeg']:
         raise ParamExcepiton('Illegal file')
     
-    # md5 = hashlib.md5()
-    # while True:
-    #     data = file.read(8192)
-    #     if not data:
-    #         break
-    #     md5.update(data)
-    # file_md5 = md5.hexdigest()
+    md5 = hashlib.md5()
+    while True:
+        data = file.read(8192)
+        if not data:
+            break
+        md5.update(data)
+    file_md5 = md5.hexdigest()
 
-    # new_filename = file_md5 + ext
-    file.save(os.path.join(dir_path, file.filename))
-    return file.filename
+    new_filename = file_md5 + ext
+    file.seek(0)
+    file.save(os.path.join(dir_path, new_filename))
+    return new_filename
